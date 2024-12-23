13f2ff3d9a939278275e95dfcd75c29a06dd674b
HydrogenSulfate <490868991@qq.com>
2024-12-23 10:43:49 +0800

    update static prim erro code

--- deepmd/pd/utils/env.py
+++ deepmd/pd/utils/env.py
@@ -16,6 +16,8 @@ from deepmd.env import (
     set_default_nthreads,
 )
 
+log = logging.getLogger(__name__)
+
 SAMPLER_RECORD = os.environ.get("SAMPLER_RECORD", False)
 DP_DTYPE_PROMOTION_STRICT = os.environ.get("DP_DTYPE_PROMOTION_STRICT", "0") == "1"
 try:
@@ -141,17 +143,162 @@ def enable_prim(enable: bool = True):
     ]
     EAGER_COMP_OP_BLACK_LIST = list(set(EAGER_COMP_OP_BLACK_LIST))
 
+    # NOTE: operator in list below will not use composite
+    # operator but kernel instead
+    STATIC_COMP_OP_BLACK_LIST = [
+        ##### forward decomp #####
+        # "pd_op.any",
+        # "pd_op.mean",
+        # "pd_op.p_norm",
+        # "pd_op.pow",
+        # "pd_op.one_hot",
+        # "pd_op.squared_l2_norm",
+        # "pd_op.reciprocal",
+        # "pd_op.bce_loss",
+        # "pd_op.bmm",
+        # "pd_op.softmax",
+        # "pd_op.log_softmax",
+        # "pd_op.stack",
+        # "pd_op.silu",
+        # "pd_op.swiglu",
+        # "pd_op.relu",
+        # "pd_op.relu6",
+        # "pd_op.squeeze",
+        # "pd_op.unsqueeze",
+        # "pd_op.add_n",
+        # "pd_op.full_like",
+        # "pd_op.gelu",
+        # "pd_op.hardsigmoid",
+        # "pd_op.hardswish",
+        # "pd_op.heaviside",
+        # "pd_op.leaky_relu",
+        # "pd_op.flatten",
+        # "pd_op.clip",
+        # "pd_op.index_select",
+        # "pd_op.square",
+        # "pd_op.sigmoid_cross_entropy_with_logits",
+        # "pd_op.mean_all",
+        # "pd_op.embedding",
+        # "pd_op.index_sample",
+        # "pd_op.elu",
+        # "pd_op.lerp",
+        # "pd_op.log_loss",
+        # "pd_op.kldiv_loss",
+        # "pd_op.softsign",
+        # "pd_op.numel",
+        # "pd_op.swish",
+        # "pd_op.addmm",
+        ##### backward decomp #####
+        # "pd_op.abs_grad",
+        "pd_op.assign_grad",
+        "pd_op.bce_loss_grad",
+        "pd_op.cumsum_grad",
+        "pd_op.cumprod_grad",
+        "pd_op.divide_grad",
+        "pd_op.floor_grad",
+        "pd_op.sum_grad",
+        "pd_op.mean_grad",
+        "pd_op.gelu_grad",
+        "pd_op.reduce_as_grad",
+        # "pd_op.reshape_grad",
+        "pd_op.roll_grad",
+        "pd_op.transpose_grad",
+        "pd_op.scatter_grad",
+        "pd_op.scatter_nd_add_grad",
+        "pd_op.sin_grad",
+        "pd_op.cos_grad",
+        "pd_op.tanh_grad",
+        "pd_op.concat_grad",
+        "pd_op.split_grad",
+        "pd_op.cast_grad",
+        "pd_op.add_grad",
+        "pd_op.subtract_grad",
+        "pd_op.multiply_grad",
+        "pd_op.elementwise_pow_grad",
+        "pd_op.pow_grad",
+        "pd_op.scale_grad",
+        "pd_op.stack_grad",
+        "pd_op.layer_norm_grad",
+        "pd_op.dropout_grad",
+        "pd_op.erf_grad",
+        "pd_op.expand_grad",
+        # "pd_op.log_grad",
+        "pd_op.square_grad",
+        # "pd_op.exp_grad",
+        "pd_op.sqrt_grad",
+        "pd_op.rsqrt_grad",
+        # "pd_op.silu_grad",
+        # "pd_op.softmax_grad",
+        "pd_op.squeeze_grad",
+        "pd_op.unsqueeze_grad",
+        # "pd_op.matmul_grad",
+        # "pd_op.maximum_grad",
+        "pd_op.masked_select_grad",
+        "pd_op.relu_grad",
+        "pd_op.gather_grad",
+        # "pd_op.gather_nd_grad",
+        "pd_op.instance_norm_grad",
+        "pd_op.pad_grad",
+        "pd_op.max_grad",
+        "pd_op.slice_grad",
+        "pd_op.tile_grad",
+        "pd_op.hardswish_grad",
+        "pd_op.leaky_relu_grad",
+        "pd_op.sigmoid_grad",
+        "pd_op.topk_grad",
+        "pd_op.batch_norm_grad",
+        "pd_op.prod_grad",
+        # "pd_op.minimum_grad",
+        "pd_op.group_norm_grad",
+        "pd_op.swiglu_grad",
+        "pd_op.softsign_grad",
+        # "pd_op.where_grad",
+        "pd_op.expm1_grad",
+        "pd_op.atan2_grad",
+        "pd_op.put_along_axis_grad",
+        "pd_op.atan_grad",
+        "pd_op.swish_grad",
+        "pd_op.fmax_grad",
+        "pd_op.fmin_grad",
+        "pd_op.dot_grad",
+        "pd_op.logcumsumexp_grad",
+        "pd_op.logsumexp_grad",
+        "pd_op.trunc_grad",
+        "pd_op.kthvalue_grad",
+        "pd_op.argsort_grad",
+        "pd_op.kron_grad",
+        # "pd_op.take_along_axis_grad",
+        "pd_op.ceil_grad",
+        "pd_op.amax_grad",
+        "pd_op.amin_grad",
+        # "pd_op.p_norm_grad",
+    ]
     """Enable running program in primitive C++ API in eager/static mode."""
     from paddle.framework import (
         core,
     )
 
-    core.set_prim_eager_enabled(enable)
-    paddle.framework.core._set_prim_all_enabled(enable)
-    if JIT is False and enable:
-        paddle.framework.core._set_prim_backward_blacklist(*EAGER_COMP_OP_BLACK_LIST)
-    log = logging.getLogger(__name__)
-    log.info(f"{'Enable' if enable else 'Disable'} prim in eager and static mode.")
+    if JIT:
+        if enable:
+            paddle.framework.core._set_prim_all_enabled(enable)
+            # pass
+            paddle.framework.core._set_prim_backward_blacklist(
+                *STATIC_COMP_OP_BLACK_LIST
+            )
+            log.info(
+                f"Add {STATIC_COMP_OP_BLACK_LIST} into static decomposite blacklist"
+            )
+    else:
+        if enable:
+            core.set_prim_eager_enabled(enable)
+            paddle.framework.core._set_prim_backward_blacklist(
+                *EAGER_COMP_OP_BLACK_LIST
+            )
+            log.info(f"Add {EAGER_COMP_OP_BLACK_LIST} into eager decomposite blacklist")
+
+    log.info(
+        f"******************** {'Enable' if enable else 'Disable'} prim in eager and static mode.********************"
+    )
 
 
 def set_random_seed(seed: int):
